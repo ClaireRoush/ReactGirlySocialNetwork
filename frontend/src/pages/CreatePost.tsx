d71a3ce437c0f0deef8d0fe8043f990268217850
froggy-jpg <meowiemeowie@proton.me>
2024-10-27 18:19:47 +0300

    fix: #80 and some design stuff

--- frontend/src/pages/CreatePost.tsx
+++ frontend/src/pages/CreatePost.tsx
@@ -1,10 +1,11 @@
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "../css/Quill.css";
 import { Navigate } from "react-router-dom";
 import Styles from "../css/CreatePost.module.css";
 import Header from "./Header";
+
 const attach = process.env.REACT_APP_STATIC_URL + "/images/attach_icon.svg";
 const api = process.env.REACT_APP_API_URL;
 const post = process.env.REACT_APP_STATIC_URL + "/images/post_button.svg";
@@ -32,7 +33,8 @@ export default function CreatePost({
   const [content, setContent] = useState<string>("");
   const [redirect, setRedirect] = useState<boolean>(false);
   const [files, setFiles] = useState<FileList | null>(null);
-
+  const [fileName, setFileName] = useState<string>('');
+  
   async function createNewPost(ev: FormEvent) {
     const data = new FormData();
     data.set("content", content);
@@ -41,7 +43,7 @@ export default function CreatePost({
     }
     ev.preventDefault();
     const token = localStorage.getItem("token");
-
+    
     const response = await fetch(`${api}/post`, {
       method: "POST",
       body: data,
@@ -53,8 +55,16 @@ export default function CreatePost({
     if (response.ok) {
       setRedirect(true);
       updatePosts((await response.json()).postDoc);
+      setFileName("")
     }
   }
+  
+  useEffect(() => {
+    if (files && files.length > 0) {
+      setFileName(files[0].name);
+    }
+  }, [files]);
+
 
   return (
     <div>
@@ -65,12 +75,13 @@ export default function CreatePost({
             <a>wanna post something? :3</a>
           </section>
           <section className={Styles.attachWrapper}>
+            <div className={Styles.fileName}>{fileName}</div>
             <img src={attach} className={Styles.attach_img}></img>
             <input
               type="file"
               onChange={(ev) => setFiles(ev.target.files)}
               className={Styles.attach_button}
-            ></input>
+              ></input>
           </section>
           <ReactQuill
             value={content}
