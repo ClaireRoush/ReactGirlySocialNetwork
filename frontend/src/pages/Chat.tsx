3120902afc1e2fe39b6e131b02ed147cdfc1add0
ClaireRoush <ClaireRoush13iq@gmail.com>
2024-10-28 03:25:00 +0200

    fix: add zero margin for main html body

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -133,8 +133,6 @@ export default function Chat() {
 
   return (
     <div>
-      <Header color={"#FFFFFF"} />
-
       <div className={Styles.wrapperForAll}>
         <section className={Styles.header}>
           {selectedContact ? (
